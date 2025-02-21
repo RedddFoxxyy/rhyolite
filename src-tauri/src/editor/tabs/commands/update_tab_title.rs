842cef777e2d2a2fe403c228918a305fca2553d5
Suyog Tandel <127536688+RedddFoxxyy@users.noreply.github.com>
2025-02-21 20:26:27 +0530

    fix: update tab title did not delete the file with old title.

--- src-tauri/src/editor/tabs/commands/update_tab_title.rs
+++ src-tauri/src/editor/tabs/commands/update_tab_title.rs
@@ -1,8 +1,11 @@
-use tauri::{AppHandle, Manager};
-use crate::app_state::{AppState, Tab};
-use crate::editor::tabs::{update_tabs_state, TabCommands};
+use std::fs;
 
-impl TabCommands{
+use crate::app_state::{AppState, Tab, TROVE_DIR};
+use crate::editor::io::get_trove_dir;
+use crate::editor::tabs::{update_tabs_state, TabCommands};
+use tauri::{AppHandle, Manager};
+
+impl TabCommands {
     pub fn update_tab_title(app: AppHandle, payload: Option<String>) {
         log::debug!("update_tab_title init");
         let Some(payload) = payload else {
@@ -15,19 +18,38 @@ impl TabCommands{
             let state = temp_app.state::<AppState>();
 
             let tabs = &mut state.tab_switcher.write().unwrap().tabs;
-            let title = payload_object.title;
+            let new_title = payload_object.title;
             let id = payload_object.id;
 
+            let mut old_title = None;
+
             // Check if the new title already exists in other tabs
-            if tabs.values().any(|tab| tab.id != id && tab.title == title) {
+            if tabs
+                .values()
+                .any(|tab| tab.id != id && tab.title == new_title)
+            {
                 log::debug!("A tab with this title already exists.");
             } else {
                 // Get the tab, update its title, and insert it back
                 if let Some(mut tab) = tabs.get(&id).cloned() {
-                    tab.title = title;
+                    old_title = Some(tab.title.clone());
+                    tab.title = new_title;
                     tabs.insert(id, tab.clone());
                 } else {
                     log::debug!("Tab not found");
+                }
+            }
+
+            // Delete the file with old title, if old title exists.
+            if old_title.is_some() {
+                let trove_dir = get_trove_dir(TROVE_DIR);
+                let filename = sanitize_filename::sanitize(format!("{}.md", old_title.unwrap()));
+                let file_path = trove_dir.join(&filename);
+
+                if file_path.exists() {
+                    let _ = fs::remove_file(&file_path).map_err(|e| {
+                        format!("Failed to delete file {}: {}", file_path.display(), e)
+                    });
                 }
             }
         }
