fa4ef1a3603e6020f88a091517e01377d7d33a2e
Suyog Tandel <127536688+RedddFoxxyy@users.noreply.github.com>
2025-03-23 01:32:42 +0530

    chore: update dependencies

--- src/lib/components/workspace.svelte
+++ src/lib/components/workspace.svelte
@@ -1,43 +1,43 @@
 <script lang="ts">
-  import { onDestroy, onMount } from "svelte";
-  import tabService from "$lib/services/tab.service";
-  import { type Tab, TabType } from "$lib/types/tab";
-  import Document from "./document.svelte";
-  import { listen } from "@tauri-apps/api/event";
-  import { getCurrentWindow } from "@tauri-apps/api/window";
-  import { invoke } from "@tauri-apps/api/core";
+	import { onDestroy, onMount } from "svelte";
+	import tabService from "$lib/services/tab.service";
+	import { type Tab, TabType } from "$lib/types/tab";
+	import Document from "./document.svelte";
+	import { listen } from "@tauri-apps/api/event";
+	import { getCurrentWindow } from "@tauri-apps/api/window";
+	import { invoke } from "@tauri-apps/api/core";
 
-  let tabs: Tab[] = $state([]);
-  let currentTab: Tab | null = $state(null);
+	let tabs: Tab[] = $state([]);
+	let currentTab: Tab | null = $state(null);
 
-  onMount(() => {
-    // Listen for the 'Tabs' event from the backend
-    const tabslisten = listen<Tab[]>("Tabs", (event) => {
-      tabs = event.payload;
-    });
-    const currentTablisten = listen<Tab>("Current_Tab", (event) => {
-      currentTab = event.payload;
-    });
-    return () => {
-      tabslisten.then((unsub) => unsub());
-      currentTablisten.then((unsub) => unsub());
-    };
-  });
+	onMount(() => {
+		// Listen for the 'Tabs' event from the backend
+		const tabslisten = listen<Tab[]>("Tabs", (event) => {
+			tabs = event.payload;
+		});
+		const currentTablisten = listen<Tab>("Current_Tab", (event) => {
+			currentTab = event.payload;
+		});
+		return () => {
+			tabslisten.then((unsub) => unsub());
+			currentTablisten.then((unsub) => unsub());
+		};
+	});
 
-  const onOpenTab = (tab: Tab) => {
-    tabService.switchTab(tab);
-  };
+	const onOpenTab = (tab: Tab) => {
+		tabService.switchTab(tab);
+	};
 </script>
 
 <div class="flex grow justify-center mt-[30px] px-10 overflow-auto">
-  <!-- {#each tabs as tab} -->
-  <!-- {#if tab.tabType === TabType.Document || tab.tabType === undefined} -->
-  <!-- TODO: Q: How to switch between tabs? -->
-  <!-- 1: Have all tabs as separate DOM Elements, set display:none on inactive tabs -->
-  <!--    Pro: possibly retained DOM states. Con: Too large DOM-->
-  <!-- 2: Have only active tab in DOM -->
-  <!--    Pro: possibly retained DOM states. Con: Too large DOM-->
-  <Document />
-  <!-- {/if} -->
-  <!-- {/each} -->
+	<!-- {#each tabs as tab} -->
+	<!-- {#if tab.tabType === TabType.Document || tab.tabType === undefined} -->
+	<!-- TODO: Q: How to switch between tabs? -->
+	<!-- 1: Have all tabs as separate DOM Elements, set display:none on inactive tabs -->
+	<!--    Pro: possibly retained DOM states. Con: Too large DOM-->
+	<!-- 2: Have only active tab in DOM -->
+	<!--    Pro: possibly retained DOM states. Con: Too large DOM-->
+	<Document />
+	<!-- {/if} -->
+	<!-- {/each} -->
 </div>
